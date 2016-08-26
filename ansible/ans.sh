9ca0c121c6cf72542765e3ecfd5b9c7cd1f8240e
Ranjith <rchirakk@gmail.com>
2016-08-26 16:15:09 -0700

    ansible ping

---
+++ ansible/ans.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+key=$(vagrant ssh-config | grep -w IdentityFile | grep -o "/home.*")
+ANSHOST=ans.hosts
+echo "[hosts]" > $ANSHOST
+echo "192.168.33.10" >> $ANSHOST
+echo $key $ANSHOST
+ansible hosts -u vagrant --private-key=$key -i $ANSHOST -m ping
