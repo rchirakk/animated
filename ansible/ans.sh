ac30f661b0e5acb249aad925d16c89975fd1501e
Ranjith <rchirakk@gmail.com>
2016-09-30 10:04:18 -0700

    added bash dir

--- ansible/ans.sh
+++ ansible/ans.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-key=$(vagrant ssh-config | grep -w IdentityFile | grep -o "/home.*")
+
+key=$(vagrant ssh-config | grep -w IdentityFile | grep -o "/home.*" | head -n1)
 ANSHOST=ans.hosts
 echo "[hosts]" > $ANSHOST
 echo "192.168.33.10" >> $ANSHOST
 echo "192.168.33.11" >> $ANSHOST
-echo $key $ANSHOST
+echo "key=$key"
 ansible hosts -u vagrant --private-key=$key -i $ANSHOST -m ping
 ansible hosts -u vagrant --private-key=$key -i $ANSHOST -m shell -a "ls -l /"
